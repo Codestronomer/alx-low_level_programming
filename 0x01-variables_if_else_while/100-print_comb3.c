dc046fe53c3a1cb0be449f58d53ca2635c6261f7
John Rumide <johnrumide6@gmail.com>
2022-04-08 20:57:03 -0700

    add advanced tasks file

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,21 +8,24 @@
  */
 int main(void)
 {
-	int d;
+int d, e;
 
-	for (d = 0; d < 90; d++)
-	{
-		for ()
-		{
-			putchar((d / 10) + '0');
-			putchar((d % 10) + '0');
-		}
-		if (d != 89)
-		{
-			putchar(',');
-			putchar(' ');
-		}
-	}
-	putchar('\n');
-	return (0);
+for (d = 0; d < 9; d++)
+{
+for (e = d + 1; e < 10; e++)
+{
+putchar((d % 10) + '0');
+putchar((e % 10) + '0');
+}
+if (d == 8 && e == 9)
+{
+continue;
+}
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+
+return (0);
 }
