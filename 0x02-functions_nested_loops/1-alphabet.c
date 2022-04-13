59dd95151b7987079406520dc50aab19b10e59af
John Rumide <johnrumide6@gmail.com>
2022-04-12 18:08:08 -0700

    create 1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - prints the alphabets in lowercase
+ *
+ * Return: always 0 (Success)
+ */
+void print_alphabet(void);
+
+int main(void)
+{
+print_alphabet()
+return (0);
+}
+void print_alphabet(void)
+{
+int a, z = 122;
+for(a=97; a<=122; a++)
+{
+_putchar(a);
+}
+_putchar('\n');
+ return;
+}
