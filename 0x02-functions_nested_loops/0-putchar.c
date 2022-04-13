705e27e84dccde665e5684e254632d22d71d6d34
John Rumide <johnrumide6@gmail.com>
2022-04-12 17:20:47 -0700

    create 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main()
+{
+write("_putchar\n");
+
+return 0;
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+# ALX LOW LEVEL PROGRAMMING
+## 0x02 C - Functions and nested loops tasks
