9b6f4b80ff9ca165be95daf1b9d509f09615470c
John Rumide <johnrumide6@gmail.com>
2022-04-12 17:37:25 -0700

    fix 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,14 @@
-#include <stdio.h>
+#include "main.h"
 #include <unistd.h>
 
 int main()
 {
-write("_putchar\n");
-
+int i;
+int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
+for (i = 0; i < 9; i++)
+{ 
+_putchar(str[i])
+}
+_putchar("\n")
 return 0;
 }
