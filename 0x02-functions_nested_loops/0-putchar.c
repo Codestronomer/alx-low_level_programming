1dc9c470b7f4b025325c4d06f00362edb704754f
John Rumide <johnrumide6@gmail.com>
2022-04-12 17:56:55 -0700

    bug fixes

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,20 @@
 #include "main.h"
 #include <unistd.h>
 
-int main()
+/*
+ * main - Prints _putchar as a message
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-int i;
+int i, j;
 int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
-for (i = 0; i < 9; i++)
-{ 
+j = sizeof(str) / sizeof(int);
+for (i = 0; i < j; i++)
+{
 _putchar(str[i]);
 }
 _putchar('\n');
-return 0;
+return (0);
 }
