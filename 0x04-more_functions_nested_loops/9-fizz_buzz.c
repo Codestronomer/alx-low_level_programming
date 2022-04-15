62bf1e9ca20aa0aacb6382418289d27bf041841b
Codestronomer <johnrumide6@gmail.com>
2022-04-15 03:47:34 +0100

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,4 +1,3 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
@@ -12,29 +11,29 @@ int main(void)
 int i;
 
 i = 1;
-_putchar('0' + i);
-_putchar(32);
+putchar('0' + i);
+putchar(32);
 for (i = 2; i < 101; i++)
 {
 if ((i % 3 == 0) && (1 % 5 == 0))
 {
-_putchar('FizzBuzz');
-_putchar(32);
+putchar('FizzBuzz');
+putchar(32);
 }
 else if (i % 3 == 0)
 {
-_putchar('Fizz');
-_putchar('Buzz');
+putchar('Fizz');
+putchar('Buzz');
 }
 else if (i % 5 == 0)
 {
-_putchar('Buzz');
-_putchar(32);
+putchar('Buzz');
+putchar(32);
 }
 else
 {
-_putchar('0' + i);
-_putchar(32);
+putchar('0' + i);
+putchar(32);
 }
 }
 return (0);
