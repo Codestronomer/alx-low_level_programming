86ac70f0041d5c39e322fdcdc1711e37c79c4169
John Rumide <johnrumide6@gmail.com>
2022-04-14 18:51:51 -0700

    fix task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,11 +11,11 @@
 void most_numbers(void)
 {
 int i, j;
-for (i = 0; i < 11; i++)
+for (i = 48; i < 59; i++)
 {
-for (j = 0; j < 15; j++)
+for (j = 48; j < 65; j++)
 {
-_putchar('0' + j);
+_putchar(j);
 }
 }
 _putchar('\n');
