86a0e45349f14b5caa8bee4882047369c94d9e5c
Codestronomer <johnrumide6@gmail.com>
2022-04-15 03:44:33 +0100

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-#include <unistd.h>
 
 /**
  * main - FizzBuzz
@@ -8,31 +7,34 @@
  * Return: Always (0) Success
  */
 
-int main (void)
+int main(void)
 {
 int i;
 
-for (i = 0; i < 101; i++)
+i = 1;
+_putchar('0' + i);
+_putchar(32);
+for (i = 2; i < 101; i++)
 {
-if (i % 3 == 0 && 1 % 5 == 0)
+if ((i % 3 == 0) && (1 % 5 == 0))
 {
-putchar('FizzBuzz');
-putchar(32);
+_putchar('FizzBuzz');
+_putchar(32);
 }
 else if (i % 3 == 0)
 {
-putchar('Fizz');
-putchar('Buzz');
+_putchar('Fizz');
+_putchar('Buzz');
 }
 else if (i % 5 == 0)
 {
-putchar('Buzz');
-putchar(32);
+_putchar('Buzz');
+_putchar(32);
 }
 else
 {
-putchar('0' + i);
-putchar(32);
+_putchar('0' + i);
+_putchar(32);
 }
 }
 return (0);
