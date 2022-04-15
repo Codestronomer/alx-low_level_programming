63f7f75a4103ab20f6533696b150430a9b370643
John Rumide <johnrumide6@gmail.com>
2022-04-14 19:07:55 -0700

    fix task 5

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -3,19 +3,15 @@
 
 /**
  * print_line - draws a straight line
- * @n - number of times to print the _ chararacter
+ * @n: number of times to print the _ chararacter
  * Return: Always (0)
  */
 
 void print_line(int n)
 {
-int i;
-if (n > 0)
-{
-for (i = 0; i < n + 1; i++)
+for (i = 0; i < n; i++)
 {
 _putchar('_');
-}
 }
 _putchar('\n');
 }
