29d5c52ef9662313661a1c6efc9a5b9f4594333f
Codestronomer <johnrumide6@gmail.com>
2022-04-24 00:44:31 +0100

    Create 6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * cap_string - Capitalize all words in a string
+ * @s: string
+ * Return: string
+*/
+
+char *cap_string(char *s)
+{
+    int i, j;
+    char sep[] = {',',';','.','!','?','"','(',')','{','}',' ','\t','\n'};
+    for (i = 0; s[i] != '\0'; i++)
+    {
+        for (j = 0; j < 13; j++)
+        {
+            if (s[i] == sep[j])
+            {
+                if (s[i + 1] > 96 && s[i + 1] < 123)
+                {
+                    s[i + 1] = *(s + i + 1) - 32;
+                }
+            }
+        }
+    }
+
+    return (s);
+}
