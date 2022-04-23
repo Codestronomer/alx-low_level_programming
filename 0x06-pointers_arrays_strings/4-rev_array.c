585cdc6f26b0fb12b6dc786c7c6cdfca49d085af
Codestronomer <johnrumide6@gmail.com>
2022-04-24 00:40:31 +0100

    Create 4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+/**
+ * reverse_array - Reverses the content of an array of integers
+ * @a - an array of integers
+ * @n - the number of elements to swap
+ * Return: nothing
+*/
+
+void reverse_array(int *a, int n)
+{
+    int i;
+    int b[100];
+    for (i = 0; i < n; i++)
+    {
+        b[i] = a[n - i - 1];
+    }
+    for (i = 0; i < n; i++)
+    {
+        a[i] = b[i];
+    }
+
+    return (a);
+}
