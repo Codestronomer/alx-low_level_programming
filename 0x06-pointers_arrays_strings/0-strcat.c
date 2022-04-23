c931de93b22caa1f3c436363d50b42ea46cd0eba
John Rumide <johnrumide6@gmail.com>
2022-04-23 16:29:38 -0700

    create 0x06-directory, add header file

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * *_strcat - concatenates two strings
+ * @dest - destination parameter
+ * @src - source parameter
+ * Return - concatenated string
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	char res[];
+	return
+}
