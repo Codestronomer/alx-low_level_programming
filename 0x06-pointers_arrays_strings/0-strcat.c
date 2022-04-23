fba6a3423411b932eb36bf84fc1823f7460fa205
Codestronomer <johnrumide6@gmail.com>
2022-04-24 00:59:02 +0100

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,9 +3,9 @@
 
 /**
  * *_strcat - concatenates two strings
- * @dest - destination parameter
- * @src - source parameter
- * Return - concatenated string
+ * @dest: destination parameter
+ * @src: source parameter
+ * Return: concatenated string
 */
 
 char *_strcat(char *dest, char *src)
