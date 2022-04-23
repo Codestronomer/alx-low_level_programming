43ca55894b982c492f582cf86c3c99e1a4d1d570
Codestronomer <johnrumide6@gmail.com>
2022-04-24 00:50:54 +0100

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,15 +10,15 @@
 
 char *_strcat(char *dest, char *src)
 {
-    int i;
-    int dest_len = strlen(dest);
-    int src_len = strlen(src);
+int i;
+int dest_len = strlen(dest);
+int src_len = strlen(src);
 
-    for (i = 0; i < src_len && src[i] != '\0'; i++)
-    {
-        dest[dest_len + i] = *(src + i);
-    }
-    dest[dest_len + i] = '\0';
+for (i = 0; i < src_len && src[i] != '\0'; i++)
+{
+dest[dest_len + i] = *(src + i);
+}
+dest[dest_len + i] = '\0';
 
-    return (dest);
+return (dest);
 }
