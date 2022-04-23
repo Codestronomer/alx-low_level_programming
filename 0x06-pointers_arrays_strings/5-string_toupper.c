caae423098d274b2a42c95396a6925077c8b4422
Codestronomer <johnrumide6@gmail.com>
2022-04-24 00:42:05 +0100

    Create 5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ * @s - string
+ * Return: returns uppercase string
+*/
+
+char *string_toupper(char *s)
+{
+    int i;
+    int s_len = strlen(s);
+
+    for (i = 0; i < s_len; i++)
+    {
+        if (s[i] > 96 && s[i] < 123)
+        {
+           s[i] = *(s + i) - 32;
+        }
+    }
+    //s[s_len] = "\n";
+    return (s);
+}
