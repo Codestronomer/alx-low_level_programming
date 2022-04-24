00f45730d3f316d2b4f4c455ee511a2625f6410b
Codestronomer <johnrumide6@gmail.com>
2022-04-24 01:45:00 +0100

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -14,9 +14,10 @@ char *_strncpy(char *dest, char *src, int n)
 	int i;
 	int dest_len = strlen(dest);
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = *(src + i);
-	dest[dest_len + i] = '\n';
+	for ( ; i < n; i++)
+		dest[i] = '\0';
 
 	return (dest);
 }
