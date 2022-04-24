8e937c20bb850593833bd23d117a9dcde9453288
Codestronomer <johnrumide6@gmail.com>
2022-04-24 20:51:58 +0100

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -3,22 +3,22 @@
 
 /**
  * string_toupper - changes all lowercase letters of a string to uppercase
- * @s - string
+ * @s: string
  * Return: returns uppercase string
 */
 
 char *string_toupper(char *s)
 {
-    int i;
-    int s_len = strlen(s);
+        int i;
+        int s_len = strlen(s);
 
-    for (i = 0; i < s_len; i++)
-    {
-        if (s[i] > 96 && s[i] < 123)
+        for (i = 0; i < s_len; i++)
         {
-           s[i] = *(s + i) - 32;
+                if (s[i] > 96 && s[i] < 123)
+                {
+                        s[i] = *(s + i) - 32;
+                }
         }
-    }
-    //s[s_len] = "\n";
-    return (s);
+
+        return (s);
 }
