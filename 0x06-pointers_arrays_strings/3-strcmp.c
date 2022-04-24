bc20a63687c91b339f53737a15906493096785bd
Codestronomer <johnrumide6@gmail.com>
2022-04-24 02:25:09 +0100

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,9 +14,9 @@ int *_strcmp(char *s1, char *s2)
 	
 	while(res == 0)
 	{
-		if ((s1[i] == '\0') && (s2[i] == '\0'))
+		if (*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
 			break;
-		res = s1[i] - s2[i];
+		res = *(s1 + i) - *(s2 + i);
 		i++;
 	}
 
