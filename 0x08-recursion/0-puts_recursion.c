6bb6f2720b80cc26fcf383de840cb60d835e3ad8
John Rumide <johnrumide6@gmail.com>
2022-04-28 18:02:43 -0700

    fix task 1

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,12 +9,11 @@
 
 void _puts_recursion(char *s)
 {
-	int i = 0;
-
-	while (s[i] != '\0')
+	if (*s != '\0')
 	{
-		_putchar(s[i]);
-		_puts_recursion(s[i + 1]);
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
-	_putchar('\n')
+	else
+		_putchar('\n')
 }
