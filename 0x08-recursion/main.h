fa49cc1f4abc183718fdab9f041b51af2047da8d
John Rumide <johnrumide6@gmail.com>
2022-04-28 18:04:31 -0700

    fix task 1

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 
+int _putchar(char);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
@@ -10,6 +11,5 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
-int _putchar(char);
 
 #endif
