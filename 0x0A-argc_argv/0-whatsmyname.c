e1361e7db71eff72aadca848541452a3883cf1f7
John Rumide <johnrumide6@gmail.com>
2022-05-03 15:59:44 -0700

    move 0-whatsmyname.c to 0x0A

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints its name, followed by a new line
+ * @argc: number of arguments
+ * @argv: an array of arguments
+ * Return: Always (0)
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s", argv[0]);
+
+	return (0);
+}
