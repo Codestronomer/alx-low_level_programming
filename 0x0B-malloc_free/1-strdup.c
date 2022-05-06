91d4cd0a30e59f7947824aa499e45e65d4446465
John Rumide <johnrumide6@gmail.com>
2022-05-05 19:57:12 -0700

    changes to 0-create_array.c, add 1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - Returns a pointer to a newly allocated
+ * space in memory, which contains a copy of the string
+ * given as a parameter
+ * @str: string to be duplicated
+ * Return: NULL if str = NULL, pointer to string copy on success
+ */
+
+char *_strdup(char *str)
+{
+	int *s;
+	int i;
+	int len = strlen(str);
+
+	if (str == NULL)
+		return (NULL);
+	s = malloc(sizeof(char) * len);
+	if (s == NULL)
+		return (NULL);
+	for (i = 0; i < len; i++)
+	{
+		*(s + i) = str[i];
+	}
+	*(s + i) = '\0';
+	free(s);
+	return (s);
+}
