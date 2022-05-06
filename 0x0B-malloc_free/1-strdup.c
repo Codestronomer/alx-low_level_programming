f5aadd9541cba02b39540900bea7af484ecd5b3b
Codestronomer <johnrumide6@gmail.com>
2022-05-06 04:06:54 +0100

    Update 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -19,13 +19,12 @@ char *_strdup(char *str)
 
 	if (str == NULL)
 		return (NULL);
-	s = malloc(sizeof(char) * len);
+	s = (char *)malloc(sizeof(char) * len);
 	if (s == NULL)
 		return (NULL);
 	for (i = 0; i < len; i++)
 	{
 		*(s + i) = str[i];
 	}
-	*(s + i) = '\0';
 	return (s);
 }
