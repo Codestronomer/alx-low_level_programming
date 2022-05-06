fe499d4856fa2d825df822abe2c56f96672a8e77
Codestronomer <johnrumide6@gmail.com>
2022-05-06 04:40:59 +0100

    Update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,18 +18,19 @@ char *str_concat(char *s1, char *s2)
 	char *con_str;
 
 	if (s1 == NULL)
-		s1 = '';
+		s1 = "";
 	if (s2 == NULL)
-		S2 = '';
+		S2 = "";
 
 	for (i = 0; s1[i] != '\0'; i++)
 		;
 	for (j = 0; s2[j] != '\0'; j++)
 		;
 	sum = i + j;
-	con_str = (char *)malloc(sizeof(char) * sum);
+	con_str = (char *)malloc(sizeof(char) * (sum + 1));
 
 	if (con_str == NULL)
+		free(con_str);
 		return (NULL);
 
 	for (i = 0; s1[i] != '\0'; i++)
