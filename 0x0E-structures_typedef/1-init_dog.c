1e416379cf2f96557e186312c0d4e5c1f999e500
John Rumide <johnrumide6@gmail.com>
2022-05-12 18:17:12 -0700

    fix task 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -11,15 +11,10 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	struct dog *ptr;
-
-	ptr = malloc(sizeof(struct dog));
-	if (ptr == NULL)
-		return (NULL);
-	ptr = d;
-	if (ptr == NULL)
-		return (NULL);
-	ptr->name = name;
-	ptr->age = age;
-	ptr->owner = owner;
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
