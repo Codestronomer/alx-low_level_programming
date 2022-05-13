9987d00ac93a22a2234d3d397879d0eabd3bea4e
John Rumide <johnrumide6@gmail.com>
2022-05-12 17:51:46 -0700

    add README.md, dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a structure with elements of a dog
+ * @name: name of dog
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
