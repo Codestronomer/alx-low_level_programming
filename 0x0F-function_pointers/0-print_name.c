a713646ccc3e548217e6534667dea93400e01369
John Rumide <johnrumide6@gmail.com>
2022-05-13 18:18:33 -0700

    add REAME.md, main.h, 0-pirnt_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_name - prints a name
+ * @name: name to print
+ * @f: pointer to function
+ * Return: Always nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,5 @@
+# ALX LOW LEVEL PROGRAMMING
+
+##Topic: 0X0F. C - Function pointers
+
+
