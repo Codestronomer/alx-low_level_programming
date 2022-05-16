a076602ad93b8e69706e5354cf6bd61816306e17
Codestronomer <johnrumide6@gmail.com>
2022-05-16 03:14:59 +0100

    Update 3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -18,9 +18,9 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i = 0;
 
-	while (i < 5)
+	while (ops[i].op)
 	{
-		if (s[0] == *(ops[i]).op)
+		if (s[0] == ops[i].op)
 			return (ops[i].f);
 		i++;
 	}
