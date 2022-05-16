4ea53c1e652629a330868cd84890a2134f63df85
John Rumide <johnrumide6@gmail.com>
2022-05-15 19:06:09 -0700

    add 3-calc.h, 3-main.c, 3-get_op_func.c, 3-op_functions.c

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,41 @@
+#include "3-main.c"
+#include <stdlib.h>
+
+/**
+ * main - calculator
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0 success
+ */
+
+int main(int argc, char *argv[])
+{
+	int (*func)(int, int);
+	int res;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if ((argv[2] == '/' || argv[2] == '%') && (atoi(argv[3]) == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	func = get_op_func(argv[2]);
+
+	if (!func)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	res = func(atoi(argv[1]), atoi(argv[3]));
+
+	printf("%d\n", res);
+
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - sum of two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: Sum of a and b
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtraction of two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: Subtraction of a from b
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplication of two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: Product of a and b
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - division of two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: division of a by b
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - modulos of two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: remainder of the division of a by b
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
