88cfc6f2bb74b373e6c0abfcd572635161e242a3
Codestronomer <johnrumide6@gmail.com>
2022-05-16 04:19:33 +0100

    Update 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -30,13 +30,18 @@ int main(int argc, char *argv[])
 		exit(100);
 	}
 
-	func = get_op_func(argv[2]);
-
-	if (!func)
+	if ((*argv[2] != '+' &&
+		*argv[2] != '-' &&
+		*argv[2] != '/' &&
+		*argv[2] != '*' &&
+		*argv[2] != '%')
+		|| argv[2][1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
+
+	func = get_op_func(argv[2]);
 
 	res = func(a, b);
 
