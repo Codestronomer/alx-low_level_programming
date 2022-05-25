234ddd59b73d4059674f40fde0110aa098305b8d
Codestronomer <johnrumide6@gmail.com>
2022-05-25 05:53:31 +0100

    Update 4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -11,11 +11,11 @@ void free_list(list_t *head)
 {
 	list_t *temp;
 
-	while (head)
+	while (head != NULL)
 	{
-		temp = head;
-		free(temp->str);
-		free(temp);
-		head = head->next;
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
 	}
 }
