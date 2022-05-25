39a57f5aa9c5e72ee0cffd3852a67eea1ad240e9
John Rumide <johnrumide6@gmail.com>
2022-05-24 20:41:44 -0700

    add README.md, 0-print_List.c, lists.h

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_list - prints all elemets of a list_t list
+ * @h: first node in the list
+ *
+ * Return: the number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	int i = 0;
+
+	while(h)
+	{
+		if (h->str)
+		{
+			printf("[%d] %s", len, str);
+		}
+		else {
+			printf("[0] (nil)");
+		}
+		h = h->next;
+		i++;
+	}
+
+	return (i);
+}
