5a49c627e819754b1d7c319827e9231262b4fb7d
John Rumide <johnrumide6@gmail.com>
2022-05-24 20:59:27 -0700

    add 1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * list_len - returns the number of elements in a
+ * linked list_t list
+ * @h: first node in the list
+ *
+ * Return: the number of nodes
+ */
+
+size_t list_len(const list_t *h)
+{
+	int i = 0;
+
+	while (h)
+	{
+		h = h->next;
+		i++;
+	}
+
+	return (i);
+}
