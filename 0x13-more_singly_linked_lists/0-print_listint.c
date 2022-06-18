e8fb064fcbedee057d4b52743a76db65aa82e093
John Rumide <johnrumide6@gmail.com>
2022-06-17 17:08:49 -0700

    add 0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * print_listint - prints all elemenets of a
+ * listint_t *h
+ * @h: head
+ *
+ * Return: Number of nodes
+ */
+
+int print_listint(const listint_t *h)
+{
+	int i;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		i++;
+		h = h->next;
+	}
+
+	return (i);
+}
+
