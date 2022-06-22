dac43561882564b68762e32b7397665dc48e707d
John Rumide <johnrumide6@gmail.com>
2022-06-21 18:15:10 -0700

    fix;

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -16,12 +16,9 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i;
 
-	if (head != NULL)
+	for (i = 0; head != NULL && i < index; i++)
 	{
-		for (i = 0; i < index; i++)
-		{
-			head = head->next;
-		}
+		head = head->next;
 	}
 	return (head);
 }
