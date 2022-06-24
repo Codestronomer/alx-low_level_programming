80ad0a8dedc13e1647a811462565ccb57b0c4284
John Rumide <johnrumide6@gmail.com>
2022-06-23 18:07:42 -0700

    add more declarations to lists.h

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -14,11 +14,11 @@ listint_t *reverse_listint(listint_t **head)
 	listint_t *prev = NULL;
 	listint_t *next = NULL;
 
-	while (*head)
+	while (*head != NULL)
 	{
 		next = (*head)->next;
 		(*head)->next = prev;
-		prev = (*head);
+		prev = *head;
 		*head = next;
 	}
 	*head = prev;
