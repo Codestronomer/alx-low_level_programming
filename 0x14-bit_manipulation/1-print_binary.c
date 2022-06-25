e35a19cfc51c5590988e41239185e26ac3507cd7
John Rumide <johnrumide6@gmail.com>
2022-06-24 17:59:14 -0700

    add 1-print_binary.c, add _putchar func to main.h:

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary representation
+ * of a number
+ * @n: number to be converted to binary
+ *
+ * Return: always Nothing
+ */
+
+void print_binary(unsigned long int n)
+{
+	if (n == 0)
+		_putchar("0");
+	while (n > 0)
+	{
+		if ((n & 1) == 0)
+			_putchar('0');
+		else
+			_putchar('1');
+		n = n >> 1;
+	}
+}
