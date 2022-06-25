542e107443f094ccc2fb4460ea5f6c9e63fb2f0c
John Rumide <johnrumide6@gmail.com>
2022-06-24 18:09:39 -0700

    [fix] using recursion

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,14 +10,12 @@
 
 void print_binary(unsigned long int n)
 {
-	if (n == 0)
+	if (!(n >> 0))
 		_putchar('0');
-	while (n > 0)
+	else
 	{
-		if ((n & 1) == 0)
-			_putchar('0');
-		else
-			_putchar('1');
-		n = n >> 1;
+		if (n >> 1)
+			print_binary(n >> 1);
+		_putchar((n & 1) + '0');
 	}
 }
