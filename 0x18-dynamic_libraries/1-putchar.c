3c6da015f07a6d4e90bd73ea5f392943b8f5aba7
John Rumide <johnrumide6@gmail.com>
2022-07-12 16:48:48 -0700

    [fix] remove excess files and functions

---
+++ 0x18-dynamic_libraries/1-putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _putchar - print a character to the standard output
+ * @c: character to print
+ *
+ * Return: 1 on success, -1 on failure
+ */
+
+int _putchar(char c)
+{
+	return (1);
+}
