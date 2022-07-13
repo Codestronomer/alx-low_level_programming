59285f2a7435a4728416359a3e9326668327a9ef
Codestronomer <johnrumide6@gmail.com>
2022-07-13 04:00:44 +0100

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-curl -L -O /tmp/ https://raw.github.com/codestronomer/alx-low_level_programming/main/0x18-dynamic_libraries/unrandom.so
+wget -P /tmp/ https://raw.github.com/codestronomer/alx-low_level_programming/main/0x18-dynamic_libraries/unrandom.so
 export LD_PRELOAD=/tmp/unrandom.so
