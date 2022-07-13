423e75d69875d8865cea5f837c1a300305d23227
John Rumide <johnrumide6@gmail.com>
2022-07-12 17:24:23 -0700

    add 100-operations.so library

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,65 @@
+#include "ops.h"
+
+/**
+ * add - adds two integers
+ * @a: integer
+ * @b: integer
+ *
+ * Return: result of operation
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+
+/**
+ * sub - subtracts two integers
+ * @a: integer
+ * @b: integer
+ *
+ * Return: result of operation
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+
+/**
+ * mul - multiplies two integers
+ * @a: integer
+ * @b: integer
+ *
+ * Return: result of operation
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+
+/**
+ * div - division of two integers
+ * @a: integer
+ * @b: integer
+ *
+ * Return: result of operation
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+
+/**
+ * mod - modulos of two integers
+ * @a: integer
+ * @b: integer
+ *
+ * Return: result of operation
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
