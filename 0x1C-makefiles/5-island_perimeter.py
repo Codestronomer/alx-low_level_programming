dd6f0343d4340d3ac72491839b6a6d47bc5c2028
John Rumide <johnrumide6@gmail.com>
2022-09-14 21:37:19 -0700

    5-island_perimeter.py

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# 5-island_perimeter.py
+""" Defines the island_perimeter function """
+
+
+def island_perimeter(grid):
+    """returns the perimeter of the island found in the grid"""
+    nrows = len(grid)
+    lenrows = len(grid[0])
+
+    height = 0
+    width = 0
+    for row in range(len(grid)):
+        for i in range(len(grid[row])):
+            if grid[row][i] == 1:
+                if (row != 0 and row != nrows) or (i != lenrows and i != 0):
+                    height += 1
+                    if grid[row][i + 1] == 1:
+                        width += 1
+                    if grid[row + 1][i] == 1:
+                        height += 1
+                    else:
+                        if row != nrows and height != 0:
+                            height -= 1
+    result = 2 * (height * width)
+    return result
